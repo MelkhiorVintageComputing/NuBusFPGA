288d955998bcda06bec9d7b0863df83925736900
Romain Dolbeau <romain@dolbeau.org>
2023-11-18 10:06:51 +0100

    move some of board platform stuff to common

--- nubus-to-ztex-gateware/ztex213_nubus.py
+++ nubus-to-ztex-gateware/ztex213_nubus.py
@@ -17,49 +17,9 @@ from litex.build.generic_platform import *
 from litex.build.xilinx import XilinxPlatform
 from litex.build.openocd import OpenOCD
 
+from VintageBusFPGA_Common.ztex_21x_common import ZTexPlatform
+
 # IOs ----------------------------------------------------------------------------------------------
-
-# FPGA daughterboard I/O
-
-_io = [
-    ## 48 MHz clock reference
-    ("clk48", 0, Pins("P15"), IOStandard("LVCMOS33")),
-    ## embedded 256 MiB DDR3 DRAM
-    ("ddram", 0,
-        Subsignal("a", Pins("C5 B6 C7 D5 A3 E7 A4 C6", "A6 D8 B2 A5 B3 B7"),
-            IOStandard("SSTL135")),
-        Subsignal("ba",    Pins("E5 A1 E6"), IOStandard("SSTL135")),
-        Subsignal("ras_n", Pins("E3"), IOStandard("SSTL135")),
-        Subsignal("cas_n", Pins("D3"), IOStandard("SSTL135")),
-        Subsignal("we_n",  Pins("D4"), IOStandard("SSTL135")),
-#        Subsignal("cs_n",  Pins(""), IOStandard("SSTL135")),
-        Subsignal("dm", Pins("G1 G6"), IOStandard("SSTL135")),
-        Subsignal("dq", Pins(
-            "H1 F1 E2 E1 F4 C1 F3 D2",
-            "G4 H5 G3 H6 J2 J3 K1 K2"),
-            IOStandard("SSTL135"),
-            Misc("IN_TERM=UNTUNED_SPLIT_40")),
-        Subsignal("dqs_p", Pins("H2 J4"),
-            IOStandard("DIFF_SSTL135"),
-            Misc("IN_TERM=UNTUNED_SPLIT_40")),
-        Subsignal("dqs_n", Pins("G2 H4"),
-            IOStandard("DIFF_SSTL135"),
-            Misc("IN_TERM=UNTUNED_SPLIT_40")),
-        Subsignal("clk_p", Pins("C4"), IOStandard("DIFF_SSTL135")),
-        Subsignal("clk_n", Pins("B4"), IOStandard("DIFF_SSTL135")),
-        Subsignal("cke",   Pins("B1"), IOStandard("SSTL135")),
-        Subsignal("odt",   Pins("F5"), IOStandard("SSTL135")),
-        Subsignal("reset_n", Pins("J5"), IOStandard("SSTL135")),
-        Misc("SLEW=FAST"),
-    ),
-    ("config_spiflash", 0,
-        Subsignal("cs_n", Pins("L13")),
-        # Subsignal("clk",  Pins("E9")), # 'E9' isn't a user pin, access clock via STARTUPE2 primitive, disabling the pads should do it in LiteSPIClkGen ?
-        Subsignal("mosi", Pins("K17")),
-        Subsignal("miso", Pins("K18")),
-        IOStandard("LVCMOS33"),
-    ),
-]
 
 # NuBusFPGA I/O
 
@@ -248,31 +208,23 @@ def rmii_eth_extpmod_io(extpmod):
          Subsignal("crs_dv",  Pins(f"{extpmod}:6")),
          Subsignal("tx_en",   Pins(f"{extpmod}:2")),
          Subsignal("tx_data", Pins(f"{extpmod}:0 {extpmod}:1")),
-         Subsignal("mdc",     Pins(f"{extpmod}:4")),
-         Subsignal("mdio",    Pins(f"{extpmod}:7")),
+         #Subsignal("mdc",     Pins(f"{extpmod}:4")),
+         #Subsignal("mdio",    Pins(f"{extpmod}:7")),
          Subsignal("rx_er",   Pins(f"{extpmod}:9")),
          Subsignal("int_n",   Pins(f"{extpmod}:5")),
          IOStandard("LVCMOS33"),
          ),
+         ("sep_mdc",  0, Pins(f"{extpmod}:4"), IOStandard("LVCMOS33")),
+         ("sep_mdio", 0, Pins(f"{extpmod}:7"), IOStandard("LVCMOS33")),
 ]
 _rmii_eth_extpmod_io_v1_2 = rmii_eth_extpmod_io("P1")
 
 # Platform -----------------------------------------------------------------------------------------
 
-class Platform(XilinxPlatform):
-    default_clk_name   = "clk48"
-    default_clk_period = 1e9/48e6
+class Platform(ZTexPlatform):
 
     def __init__(self, variant="ztex2.13a", version="V1.0"):
-        device = {
-            "ztex2.12a":  "xc7a15tcsg324-1", #untested, too small?
-            "ztex2.12b":  "xc7a35tcsg324-1", #untested
-            "ztex2.13a":  "xc7a35tcsg324-1",
-            "ztex2.13b":  "xc7a50tcsg324-1", #untested
-            "ztex2.13b2": "xc7a50tcsg324-1", #untested
-            "ztex2.13c":  "xc7a75tcsg324-2", #untested
-            "ztex2.13d":  "xc7a100tcsg324-2" #untested
-        }[variant]
+        
         nubus_io = {
             "V1.0" : _nubus_io_v1_0,
             "V1.2" : _nubus_io_v1_2,
@@ -285,31 +237,9 @@ class Platform(XilinxPlatform):
             "V1.0" : connectors_v1_0,
             "V1.2" : connectors_v1_2,
         }[version]
-        self.speedgrade = -1
-        if (device[-1] == '2'):
-            self.speedgrade = -2
         
-        XilinxPlatform.__init__(self, device, _io, connectors, toolchain="vivado")
+        ZTexPlatform.__init__(self, variant=variant, version=version, connectors=connectors)
+        
         self.add_extension(nubus_io)
-        print(nubus_nubus)
+        #print(nubus_nubus)
         self.add_extension(nubus_nubus)
-        
-        self.toolchain.bitstream_commands = \
-            ["set_property BITSTREAM.CONFIG.SPI_32BIT_ADDR No [current_design]",
-             "set_property BITSTREAM.CONFIG.SPI_BUSWIDTH 2 [current_design]",
-             "set_property BITSTREAM.CONFIG.CONFIGRATE 66 [current_design]",
-             "set_property BITSTREAM.GENERAL.COMPRESS true [current_design]",
-             "set_property BITSTREAM.GENERAL.CRC DISABLE [current_design]",
-             "set_property STEPS.SYNTH_DESIGN.ARGS.RETIMING true [get_runs synth_1]",
-             "set_property CONFIG_VOLTAGE 3.3 [current_design]",
-             "set_property CFGBVS VCCO [current_design]"
-#             , "set_property STEPS.SYNTH_DESIGN.ARGS.DIRECTIVE AreaOptimized_high [get_runs synth_1]"
-             ]
-
-    def create_programmer(self):
-        bscan_spi = "bscan_spi_xc7a35t.bit"
-        return OpenOCD("openocd_xc7_ft2232.cfg", bscan_spi) #FIXME
-
-    def do_finalize(self, fragment):
-        XilinxPlatform.do_finalize(self, fragment)
-        #self.add_period_constraint(self.lookup_request("clk48", loose=True), 1e9/48e6)
